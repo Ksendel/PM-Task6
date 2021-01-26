6fa5916687e70d94f635167e9eb5f11b78983cf5
Ksendel <oksana.karachun14@gmail.com>
2021-01-26 15:33:15 +0200

    first commit/

---
+++ task1.js
@@ -0,0 +1,417 @@
+let arr = [
+    {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+        registrationDate: "Tue, 12 Jan 2021 12: 28: 18 + 0000",
+        address:
+            {
+                street: "Kulas Light",
+                suite: "Apt. 556",
+                city: "Gwenborough",
+                zipcode: "92998-3874",
+                geo:
+                    {
+                        lat: "-37.3159",
+                        lng: "81.1496"
+                    }
+            },
+        phone: "050 866-66-66",
+        website: "hildegard.org",
+        company:
+            {
+                name: "Romaguera-Crona",
+                catchPhrase: "Multi-layered client-server neural-net",
+                bs: "harness real-time e-markets"
+            }
+    },
+    {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@melissa.tv",
+        registrationDate: "Mon, 11 Jan 2021 12: 28: 18 + 0000",
+        address:
+            {
+                street: "Victor Plains",
+                suite: "Suite 879",
+                city: "Wisokyburgh",
+                zipcode: "90566-7771",
+                geo:
+                    {
+                        lat: "-43.9509",
+                        lng: "-34.4618"
+                    }
+            },
+        phone: "050 877-77-77",
+        website: "anastasia.net",
+        company:
+            {
+                name: "Deckow-Crist",
+                catchPhrase: "Proactive didactic contingency",
+                bs: "synergize scalable supply-chains"
+            }
+    },
+    {
+        id: 3,
+        name: "Clementine Bauch",
+        username: "Samantha",
+        email: "Nathan@yesenia.net",
+        registrationDate: "Mon, 11 Jan 2020 12: 28: 18 + 0000",
+        address:
+            {
+                street: "Douglas Extension",
+                suite: "Suite 847",
+                city: "McKenziehaven",
+                zipcode: "59590-4157",
+                geo:
+                    {
+                        lat: "-68.6102",
+                        lng: "-47.0653"
+                    }
+            },
+        phone: "067 866-66-66",
+        website: "ramiro.info",
+        company:
+            {
+                name: "Romaguera-Jacobson",
+                catchPhrase: "Face to face bifurcated interface",
+                bs: "e-enable strategic applications"
+            }
+    },
+    {
+        id: 4,
+        name: "Patricia Lebsack",
+        username: "Karianne",
+        email: "Julianne.OConner@kory.org",
+        registrationDate: "Mon, 11 Jan 2021 18: 28: 18 + 0000",
+        address:
+            {
+                street: "Hoeger Mall",
+                suite: "Apt. 692",
+                city: "South Elvis",
+                zipcode: "53919-4257",
+                geo:
+                    {
+                        lat: "29.4572",
+                        lng: "-164.2990"
+                    }
+            },
+        phone: "097 866-66-68",
+        website: "kale.biz",
+        company:
+            {
+                name: "Robel-Corkery",
+                catchPhrase: "Multi-tiered zero tolerance productivity",
+                bs: "transition cutting-edge web services"
+            }
+    },
+    {
+        id: 5,
+        name: "Chelsey Dietrich",
+        username: "Kamren",
+        email: "Lucio_Hettinger@annie.ca",
+        registrationDate: "Mon, 11 Jan 2021 18: 48: 18 + 0000",
+        address:
+            {
+                street: "Skiles Walks",
+                suite: "Suite 351",
+                city: "Roscoeview",
+                zipcode: "33263",
+                geo: {
+                    lat: "-31.8129",
+                    lng: "62.5342"
+                }
+            },
+        phone: "(067) 888-88-88",
+        website: "demarco.info",
+        company:
+            {
+                name: "Keebler LLC",
+                catchPhrase: "User-centric fault-tolerant solution",
+                bs: "revolutionize end-to-end systems"
+            }
+    },
+    {
+        id: 6,
+        name: "Mrs. Dennis Schulist",
+        username: "Leopoldo_Corkery",
+        email: "Karley_Dach@jasper.info",
+        registrationDate: "Mon, 11 Jan 2021 18: 18: 18 + 0000",
+        address:
+            {
+                street: "Norberto Crossing",
+                suite: "Apt. 950",
+                city: "South Christy",
+                zipcode: "23505-1337",
+                geo:
+                    {
+                        lat: "-71.4197",
+                        lng: "71.7478"
+                    }
+            },
+        phone: "(067) 888-99-99",
+        website: "ola.org",
+        company:
+            {
+                name: "Considine-Lockman",
+                catchPhrase: "Synchronised bottom-line interface",
+                bs: "e-enable innovative applications"
+            }
+    },
+    {
+        id: 7,
+        name: "Kurtis Weissnat",
+        username: "Elwyn.Skiles",
+        email: "Telly.Hoeger@billy.biz",
+        registrationDate: "Mon, 11 Jan 2021 18: 48: 19 + 0000",
+
+
+        address:
+            {
+                street: "Rex Trail",
+                suite: "Suite 280",
+                city: "Howemouth",
+                zipcode: "58804-1099",
+                geo:
+                    {
+                        lat: "24.8918",
+                        lng: "21.8984"
+                    }
+            },
+        phone: "(068) 777-58-88",
+        website: "elvis.io",
+        company:
+            {
+                name: "Johns Group",
+                catchPhrase: "Configurable multimedia task-force",
+                bs: "generate enterprise e-tailers"
+            }
+    },
+    {
+        id: 8,
+        name: "Nicholas Runolfsdottir V",
+        username: "Maxime_Nienow",
+        email: "Sherwood@rosamond.me",
+        registrationDate: "Mon, 11 Jan 2021 12: 18: 18 + 0000",
+        address:
+            {
+                street: "Ellsworth Summit",
+                suite: "Suite 729",
+                city: "Aliyaview",
+                zipcode: "45169",
+                geo:
+                    {
+                        lat: "-14.3990",
+                        lng: "-120.7677"
+                    }
+
+            },
+        phone: "(068) 999-99-28",
+        website: "jacynthe.com",
+        company:
+            {
+                name: "Abernathy Group",
+                catchPhrase: "Implemented secondary concept",
+                bs: "e-enable extensible e-tailers"
+            }
+    },
+    {
+        id: 9,
+        name: "Glenna Reichert",
+        username: "Delphine",
+        email: "Chaim_McDermott@dana.io",
+        registrationDate: "Mon, 11 Jan 2021 12: 18: 18 + 0000",
+        address:
+            {
+                street: "Dayna Park",
+                suite: "Suite 449",
+                city: "Bartholomebury",
+                zipcode: "76495-3109",
+                geo:
+                    {
+                        lat: "24.6463",
+                        lng: "-168.8889"
+                    }
+            },
+        phone: "(068) 999-44-28",
+        website: "conrad.com",
+        company:
+            {
+                name: "Yost and Sons",
+                catchPhrase: "Switchable contextually-based project",
+                bs: "aggregate real-time technologies"
+            }
+    },
+    {
+        id: 10,
+        name: "Clementina DuBuque",
+        username: "Moriah.Stanton",
+        email: "Rey.Padberg@karina.biz",
+        registrationDate: "Mon, 11 Jan 2021 17: 28: 18 + 0000",
+        address:
+            {
+                street: "Kattie Turnpike",
+                suite: "Suite 198",
+                city: "Lebsackbury",
+                zipcode: "31428-2261",
+                geo:
+                    {
+                        lat: "-38.2386",
+                        lng: "57.2232"
+                    }
+            },
+        phone: "(067) 999-99-28",
+        website: "ambrose.net",
+        company:
+            {
+                name: "Hoeger LLC",
+                catchPhrase: "Centralized empowering task-force",
+                bs: "target end-to-end models"
+            }
+    }
+]
+
+let obj = {}
+
+//1.1
+arr.forEach(e => obj[e.id] = e)
+
+function hashMap() {
+    for (let key in obj)
+        console.log('Key', key, obj[key])
+}
+
+
+//1.2
+const getDataById = (id) => {
+    console.log(obj[id])
+}
+
+//1.3
+const getAddressById = (id) => {
+    const user = obj[id]
+    !user ? console.log(JSON.stringify(`User[${id}] - ` + 'No such user exists')) : console.log('Address: ' + JSON.stringify(user.address))
+}
+
+//1.4
+const getUserByCompany = (company) => {
+    let user = Object.entries(obj).find(value => {
+        return value[1].company.name === company
+    })
+
+    console.log(user)
+    return user
+}
+
+//1.5
+const immutable = () => {
+    for (const [key, _] of Object.entries(obj)) {
+        Object.defineProperty((obj[key]), 'id', {enumerable: true, writable: false, configurable: false,})
+    }
+}
+// immutable()
+// delete obj[10].id
+
+//1.6 v.1
+function setPhoneSetters(obj) {
+    for (const [_, value] of Object.entries(obj)) {
+        Object.defineProperty(value, '_phone', {value: value.phone, writable: true, enumerable: false})
+        Object.defineProperty(value, 'phone', {
+            set: function (v) {
+                const reg = /\(\d{3}\)-\d{3}-\d{2}-\d{2}/g
+                const validNumber = v.match(reg)
+                if (validNumber) this._phone = v
+                else throw 'Incorrect input'
+            },
+            get: function () {
+                return this._phone
+            }
+        })
+
+    }
+}
+
+function setPhone() {
+    setPhoneSetters(obj)
+    let id = prompt('Enter user ID: ')
+    let phone = obj[id].phone
+    obj[id].phone = prompt('Enter user number: ', '(123)-123-12-12')
+    alert('Old number: ' + phone + '\nNew number: ' + obj[id].phone)
+}
+
+//1.6 v.2
+function setPhoneSettersMethods(obj) {
+    for (const [_, value] of Object.entries(obj)) {
+        value.setPhone = (v) => {
+            const reg = /\(\d{3}\)-\d{3}-\d{2}-\d{2}/g
+            const validNumber = v.match(reg)
+            if (validNumber) value.phone = v
+            else throw 'Incorrect input'
+        }
+    }
+}
+
+// setPhoneSettersMethods(obj)
+// console.log(obj[10].phone)
+// obj[10].phone ='(123)-122-12-12'
+// obj[10].setPhone('(123)-122-12-12')
+// console.log(obj[10].phone)
+
+
+//2.1 - Arrays
+const array1 = [1, 2, 3, 4, 10, 12, 2, 2]
+const array2 = [3, 12, 13, 1, 2]
+
+const findAllIndex = (array, value) => {
+    const result = array.reduce((accumulator, element, index) => {
+        if (element === value) {
+            if (accumulator.first === -1) accumulator.first = index
+            else accumulator.last = index
+        }
+
+        return accumulator
+    }, {first: -1, last: -1})
+    return Object.values(result)
+}
+
+// console.log(findAllIndex(array1, 22))
+
+//2.2
+const intersection = (array1, array2) => {
+    const set = new Set(array2)
+    return array1.filter(value => set.has(value))
+}
+console.log(intersection(array1, array2))
+
+//2.3
+const newArray = (array1, array2, index) => {
+    array2.splice(index, 0, ...array1)
+    return array2
+}
+// console.log(newArray(array1, array2, 1))
+
+// 2.4
+function sortArray() {
+    alert('Check your console')
+    arr.sort((a, b) => b.id - a.id)
+    for (let key in arr) {
+        console.log(arr[key]);
+    }
+}
+
+//---//
+function btnFindAllIndex() {
+    let value = prompt('Enter value: ', '2')
+    alert(`[${array1}]\n` + findAllIndex(array1, Number(value)))
+}
+
+function btnIntersection() {
+    alert(`[${array1}]\n` + `[${array2}]\n` + (intersection(array1, array2)))
+}
+
+function btnNewArray() {
+    let index = Number(prompt('Enter index: ', '3'))
+    alert(`[${array1}]\n` + `[${array2}]\n` + (newArray(array1, array2, index)))
+}
